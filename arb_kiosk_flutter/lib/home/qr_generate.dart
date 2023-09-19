871b29c6eb146b380615784bfbbfef5ea6cb6a28
ArbSoftware <satabhishar@arbsoft.comm>
2023-09-19 11:57:55 +0530

    modified

---
+++ arb_kiosk_flutter/lib/home/qr_generate.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class QRCodeGenerater extends StatefulWidget {
+  // const QRCodeGenerater({Key? key}) : super(key: key);
+
+  @override
+  State<QRCodeGenerater> createState() => _QRCodeGeneraterState();
+}
+
+class _QRCodeGeneraterState extends State<QRCodeGenerater> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
